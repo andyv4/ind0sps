544734c7108e2213b7b6bf8dbdd38bb56b41eb3a
andyv4 <andyv4@gmail.com>
2019-01-21 09:42:42 +0700

    Updated job retry script

--- jobs/retry.php
+++ jobs/retry.php
@@ -2,7 +2,7 @@
 
 include 'config.php';
 
-$jobs = pmrs("select `id` from jobs where `status` > 1 and running < 1");
+$jobs = pmrs("select `id` from jobs where `status` != 1 and running < 1");
 echo json_encode($jobs);
 if(!$jobs) return;
 
